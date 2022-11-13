756856342f713f12ca5d0668550a5baec389a51e
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-11-13 15:46:10 +0100

    4

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,9 +1,9 @@
 import { Button } from "ui";
 
-export default function Web() {
+export default function Docs() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1>Docs</h1>
       <Button />
     </div>
   );
