7a81a04afad8849a01d4702fc37d3c093e085d90
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-11-13 22:49:58 +0100

    1.0.3 apanno

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -12,19 +12,17 @@ export default function Home() {
   };
 
   return (
-    <div
-    className="bg-white dark:bg-white min-h-screen flex flex-col justify-center items-center"
-    >
+    <div className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-white">
       <Head>
         <title>Pencil Domains</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="mx-auto w-auto bg-white px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
         <div className="text-center">
-          <h1 className="text-4xl font-bold tracking-tight text-black shadow-sm sm:text-5xl md:text-6xl">
+          <h1 className="hidden text-4xl font-bold tracking-tight text-black shadow-sm sm:text-5xl md:inline md:text-6xl">
             Welcome to
             <a
-              className="hover:text blue-900 font-extrabold text-blue-700"
+              className="hover:text blue-900 hidden font-extrabold text-blue-700 md:inline"
               href="https://twitter.com/pencildomains"
               target="_blank"
               rel="noopener noreferrer"
@@ -32,25 +30,39 @@ export default function Home() {
               &nbsp;HNS Pencil Search!
             </a>
           </h1>
-          <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
-            Search for your favorite HNS domains!
-          </p>
+
+          <h1 className="text-4xl font-bold tracking-tight text-black shadow-sm sm:text-5xl md:hidden md:text-6xl">
+            Welcome to
+            <a
+              className="hover:text blue-900 font-extrabold text-blue-700 md:hidden"
+              href="https://twitter.com/pencildomains"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              &nbsp;HNS Pencil Search!
+            </a>
+          </h1>
         </div>
-        <div className="mx-auto mt-6 grid max-w-lg gap-5 lg:max-w-xl lg:grid-cols-2">
+
+
+      
+
+        <div className="mx-auto mt-6 grid max-w-lg gap-5 lg:max-w-xl lg:grid-cols-2 hidden md:inline">
           <input
             className="
-            flex w-full
+            flex hidden
+            w-full
             items-center
-            justify-center
-            rounded-md border
-            border-transparent
-            bg-white px-8 py-3
-            text-base font-bold
-            text-black shadow-sm
+            justify-center rounded-md
+            border
+            border-transparent bg-white px-8
+            py-3 text-base
+            font-bold text-black
+            shadow-sm
             hover:bg-white
             hover:text-black
-            hover:shadow-xl
-            md:py-4 md:px-10 md:text-lg"
+            hover:shadow-xl md:inline md:py-4
+            md:px-10 md:text-lg"
             type="text"
             aria-label="Filter names"
             placeholder="Filter names..."
@@ -59,29 +71,82 @@ export default function Home() {
           />
           <button
             className="
-            flex w-full
+            flex hidden
+            w-full
             items-center
-            justify-center
-            rounded-md border
-            border-transparent
-            bg-white px-8 py-3
-            text-base font-bold
-            text-black shadow-sm
+            justify-center rounded-md
+            border
+            border-transparent bg-white px-8
+            py-3 text-base
+            font-bold text-black
+            shadow-sm
             hover:bg-white
             hover:text-black
-            hover:shadow-xl
-            md:py-4 md:px-10 md:text-lg"
+            hover:shadow-xl md:inline md:py-4
+            md:px-10 md:text-lg"
             onClick={getData}
           >
             Get Results
           </button>
         </div>
-        <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
+
+        <div className="mx-auto mt-6 grid max-w-lg gap-5 lg:max-w-xl lg:grid-cols-2 md:hidden">
+          <input
+            className="
+            flex
+            w-full
+            items-center
+            justify-center rounded-md
+            border
+            border-transparent bg-white px-8
+            py-3 text-base
+            font-bold text-black
+            shadow-sm
+            hover:bg-white
+            hover:text-black
+            hover:shadow-xl md:hidden md:py-4
+            md:px-10 md:text-lg"
+            type="text"
+            aria-label="Filter names"
+            placeholder="Filter names..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button
+            className="
+            flex
+            w-full
+            items-center
+            justify-center rounded-md
+            border
+            border-transparent bg-white px-8
+            py-3 text-base
+            font-bold text-black
+            shadow-sm
+            hover:bg-white
+            hover:text-black
+            hover:shadow-xl md:hidden md:py-4
+            md:px-10 md:text-lg"
+            onClick={getData}
+          >
+            Get Results
+          </button>
+        </div>
+        
+
+
+        <div className="hidden md:inline">
           <pre>{JSON.stringify(data, null, 6)}</pre>
         </div>
+
+        <div className="md:hidden">
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+
+
       </main>
-      <footer className="mx-auto w-auto bg-white px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
-        <div className="text-center">
+      <footer className="hidden md:inline">
+        <div className="">
           <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
             Made with ❤️ by{" "}
             <a
@@ -94,6 +159,34 @@ export default function Home() {
             </a>
           </p>
         </div>
+      </footer>
+
+      <footer className="md:hidden">
+        <div className="">
+          <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
+            Made with ❤️ by{" "}
+            <a
+              className="hover:text blue-900 font-extrabold text-blue-700"
+              href="https://twitter.com/miguelgargallo"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @miguelgargallo
+            </a>
+          </p>
+
+          <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
+            <a
+              className="hover:text blue-900 font-extrabold text-blue-700"
+              href="https://twitter.com/pencildomains"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @pencildomains
+            </a>
+          </p>
+
+        </div> 
       </footer>
     </div>
   );
