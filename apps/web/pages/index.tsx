9b2ac6d547014e2933b48009b4e4f038dd07d511
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-11-13 16:22:45 +0100

    1.0.0 Beta

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,20 +1,55 @@
-// this page will print data fetch request from https://e.hnsfans.com/api/... while "..." is the path of the request
+import React, { useState } from 'react';
+import Head from 'next/head';
 
-import { NextPage } from "next";
-import { useRouter } from "next/router";
-import { get } from "axios";
+export default function Home() {
+  const [input, setInput] = useState("");
+  const [data, setData] = useState("");
 
-const Home: NextPage = () => {
-  const router = useRouter();
-  const { path } = router.query;
-
-  if (path) {
-    get(`https://e.hnsfans.com/api/${path}`).then((res) => {
-      console.log(res.data);
-    });
+  const getData = async () => {
+    const res = await fetch(`https://e.hnsfans.com/api/names/${input}`)
+    const json = await res.json()
+    setData(json)
   }
 
-  return <h1>hnsfans.com</h1>;
-};
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-export default Home;
+      <main>
+        <h1>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p>
+          Get started by editing{' '}
+          <code>pages/index.js</code>
+        </p>
+
+        <div>
+          <div>
+            <h3>Input</h3>
+            <input value={input} onChange={(e) => setInput(e.target.value)} />
+            <button onClick={getData}>Get Data</button>
+          </div>
+          <div>
+            <h3>Data</h3>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          </div>
+        </div>
+      </main>
+
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+        </a>
+      </footer>
+    </div>
+  )
+}
