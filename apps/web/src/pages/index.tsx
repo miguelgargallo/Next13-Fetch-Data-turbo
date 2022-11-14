820bf3f1928ffab952e5a7fb838ab93031869bdf
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-11-14 19:59:39 +0100

    1.4.2 Smash 3

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         <div className="py-8">
           <div className="mx-auto max-w-7xl">
             <div className="overflow-hidden rounded-lg bg-white shadow">
-              <div className="px-4 py-5 sm:p-6">
+              <div className="px-4 py-5 text-center sm:p-6">
                 <h1 className="text-3xl font-bold leading-tight text-gray-900">
                   <a
                     href="https://hns.pencil.li"
@@ -32,29 +32,21 @@ export default function Home() {
                     Pencil Explorer
                   </a>{" "}
                 </h1>
-                <div className="mt-2 max-w-xl text-sm text-gray-500">
-                  <h4 className="text-2xl font-light leading-tight text-gray-900">
-                    Pencil Explorer is a tool to explore the Handshake
-                    blockchain, and to find out more about the domains you own.
-                  </h4>
-                </div>
+                <div className="mt-2 max-w-xl text-sm text-gray-500"></div>
                 <div className="mt-5">
-                  <label
-                    htmlFor="about"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Domain Name
-                  </label>
                   <div className="mt-1">
                     <input
+                      className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-center text-base font-bold text-black shadow-xl hover:bg-white hover:text-black hover:shadow-md md:py-4 md:px-10 md:text-lg"
                       type="text"
-                      name="about"
-                      id="about"
                       aria-label="Type and press enter, avoid caplocks"
                       placeholder="Type and press enter, avoid caplocks"
-                      className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-center text-base font-bold text-black shadow-xl hover:bg-white hover:text-black hover:shadow-md md:py-4 md:px-10 md:text-lg"
                       value={input}
                       onChange={(e) => setInput(e.target.value)}
+                      onKeyPress={(e) => {
+                        if (e.key === "Enter") {
+                          getData();
+                        }
+                      }}
                     />
                   </div>
                   <div
@@ -68,28 +60,14 @@ export default function Home() {
                       Search Results
                     </button>
                   </div>
-                  <div className="mt-2">
-                    <button
-                      type="button"
-                      className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                      onClick={getData}
-                    >
-                      Search
-                    </button>
-                  </div>
+                  <div className="mx-auto mt-6 grid max-w-lg gap-5 lg:max-w-xl lg:grid-cols-1"></div>
                 </div>
                 <div className="mt-5">
-                  <label
-                    htmlFor="about"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Domain Info
-                  </label>
                   <div className="mt-1">
                     <textarea
                       id="about"
                       name="about"
-                      rows={3}
+                      rows={20}
                       className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                       placeholder="Domain info will appear here."
                       value={JSON.stringify(data, null, 2)}
