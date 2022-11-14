a0fde9d8b9f33ceaf96269b6c6711700739a85f2
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-11-14 20:18:36 +0100

    Alpha 1.0.0/0.0.7

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -69,7 +69,7 @@ export default function Home() {
                     <textarea
                       id="about"
                       name="about"
-                      rows={20}
+                      rows={100}
                       className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                       placeholder="Domain info will appear here."
                       value={JSON.stringify(data, null, 2)}
