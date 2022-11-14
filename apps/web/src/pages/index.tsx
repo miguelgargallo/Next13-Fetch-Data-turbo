b9dd318b4c8e6e1b38c9ea2e4351d2de7b5edafe
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-11-14 13:53:10 +0100

    1.3.5 A card display the search field result with a "." at the beging as a strict charcter

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -59,11 +59,26 @@ export default function Home() {
             Search Results
           </button>
         </div>
-
         <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
-          <pre>{JSON.stringify(data, null, 6)}</pre>
+          <div className="rounded-lg shadow-lg overflow-hidden sm:mx-1 sm:flex">
+            <div className="p-4 bg-white">
+              <p className="text-base leading-6 font-semibold text-black">
+              </p>
+              <p className="mt-1 text-lg leading-5 font-semibold text-black">
+               .{data.name}
+              </p>
+            </div>
+          </div> 
         </div>
-      </main>
+        <div className="snap-y snap-mandatory">
+        <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
+          
+          <pre>{JSON.stringify(data, null, 6)}</pre>
+          <div>
+          </div>
+        </div>
+        </div>
+          </main>
       <footer className="mx-auto w-auto bg-white px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
         <div className="text-center">
           <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
