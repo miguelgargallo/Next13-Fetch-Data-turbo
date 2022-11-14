680ac931e9945369366ba683f68b2ffff02475b7
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-11-14 19:28:47 +0100

    1.4.1 Smash

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-center text-base font-bold text-black shadow-xl hover:bg-white hover:text-black hover:shadow-md md:py-4 md:px-10 md:text-lg"
             type="text"
             aria-label="Type and press enter, avoid caplocks"
-            placeholder="Type and press ente, avoid caplocks"
+            placeholder="Type and press enter, avoid caplocks"
             value={input}
             onChange={(e) => setInput(e.target.value)}
             onKeyPress={(e) => {
@@ -59,29 +59,13 @@ export default function Home() {
             Search Results
           </button>
         </div>
-        <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
-          <div className="rounded-lg shadow-lg overflow-hidden sm:mx-1 sm:flex">
-            <div className="p-4 bg-white">
-              <p className="text-base leading-6 font-semibold text-black">
-              </p>
-              <p className="mt-1 text-lg leading-5 font-semibold text-black">
-                {data.name}
-              </p>
-              <p className="mt-1 text-lg leading-5 font-semibold text-black">
-             <pre> {JSON.stringify(data, null, 6)}
-             </pre>  </p>
-            </div>
-          </div> 
-        </div>
         <div className="snap-y snap-mandatory">
-        <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
-          
-          <pre>{JSON.stringify(data, null, 6)}</pre>
-          <div>
+          <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
+            <pre>{JSON.stringify(data, null, 6)}</pre>
+            <div></div>
           </div>
         </div>
-        </div>
-          </main>
+      </main>
       <footer className="mx-auto w-auto bg-white px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
         <div className="text-center">
           <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
