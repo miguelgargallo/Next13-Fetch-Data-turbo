44d1920fcbb9f0e3953d23349ea96ce79b3790e8
Miguel Gargallo <mgargallo16@gmail.com>
2022-11-14 01:53:45 +0100

    as

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -12,45 +12,140 @@ export default function Home() {
   };
 
   return (
-    <main className="m-8 space-x-4 p-8 text-white">
-      <h1 className="m-8 p-8 text-white">
-        Welcome to
-        <a
-          className=""
-          href="https://twitter.com/pencildomains"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          &nbsp; Pencil Explorer!
-        </a>
-      </h1>
-      <div className="mx-auto mt-6 grid hidden max-w-lg gap-5 space-x-4 md:inline lg:max-w-xl lg:grid-cols-2">
-        <input
-          className="p-1 text-black"
-          type="text"
-          aria-label="Filter names"
-          placeholder="Filter names..."
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
+    <div className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-white">
+      <Head>
+        <title>Pencil Domains</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="mx-auto w-auto bg-white px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
+        <div className="text-center">
+          <h1 className="hidden text-4xl font-bold tracking-tight text-black shadow-sm sm:text-5xl md:inline md:text-6xl">
+          Welcome to
+            <a
+              className="hover:text blue-900 hidden font-extrabold text-blue-700 md:inline"
+              href="https://twitter.com/pencildomains"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              &nbsp; Pencil Explorer!
+            </a>
+          </h1>
 
-        <button
-          className="
-          
-        "
-          onClick={getData}
-        >
-          Get Results
-        </button>
-      </div>
+          <h1 className="text-4xl font-bold tracking-tight text-black shadow-sm sm:text-5xl md:hidden md:text-6xl">
+          Welcome to
+            <a
+              className="hover:text blue-900 font-extrabold text-blue-700 md:hidden"
+              href="https://twitter.com/pencildomains"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              &nbsp; Pencil Explorer!
+            </a>
+          </h1>
+        </div>
 
-      <div className="m-8">
-        <a className="text-white">{JSON.stringify(data, null, 2)}</a>
-      </div>
+        <div className="mx-auto mt-6 grid hidden max-w-lg gap-5 md:inline lg:max-w-xl lg:grid-cols-2">
+          <input
+            className="
+            flex hidden
+            w-full
+            items-center
+            justify-center rounded-md
+            border
+            border-transparent bg-white px-8
+            py-3 text-base
+            font-bold text-black
+            shadow-sm
+            hover:bg-white
+            hover:text-black
+            hover:shadow-xl md:inline md:py-4
+            md:px-10 md:text-lg"
+            type="text"
+            aria-label="Filter names"
+            placeholder="Filter names..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
 
-      <footer className="">
+          <button
+            className="
+            flex hidden
+            w-full
+            items-center
+            justify-center rounded-md
+            border
+            border-transparent bg-white px-8
+            py-3 text-base
+            font-bold text-black
+            shadow-sm
+            hover:bg-white
+            hover:text-black
+            hover:shadow-xl md:inline md:py-4
+            md:px-10 md:text-lg"
+            onClick={getData}
+          >
+            Get Results
+          </button>
+        </div>
+
+        <div className="mx-auto mt-6 grid max-w-lg gap-5 md:hidden lg:max-w-xl lg:grid-cols-2">
+          <input
+            className="
+            flex
+            w-full
+            items-center
+            justify-center rounded-md
+            border
+            border-transparent bg-white px-8
+            py-3 text-base
+            font-bold text-black
+            shadow-sm
+            hover:bg-white
+            hover:text-black
+            hover:shadow-xl md:hidden md:py-4
+            md:px-10 md:text-lg"
+            type="text"
+            aria-label="Filter names"
+            placeholder="Filter names..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button
+            className="
+            flex
+            w-full
+            items-center
+            justify-center rounded-md
+            border
+            border-transparent bg-white px-8
+            py-3 text-base
+            font-bold text-black
+            shadow-sm
+            hover:bg-white
+            hover:text-black
+            hover:shadow-xl md:hidden md:py-4
+            md:px-10 md:text-lg"
+            onClick={getData}
+          >
+            Get Results
+          </button>
+        </div>
+
+        <div className="mx-auto mt-6 hidden sm:inline">
+          <div className="">
+            <pre>{JSON.stringify(data, null, 6)}</pre>
+          </div>
+        </div>
+
+        <div className="mx-auto mt-6 sm:hidden">
+          <div className="">
+            <p>{JSON.stringify(data, null, 6)}</p>
+          </div>
+        </div>
+      </main>
+      <footer className="hidden md:inline">
         <div className="">
-          <p className="">
+          <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
             Made with ❤️ by{" "}
             <a
               className="hover:text blue-900 font-extrabold text-blue-700"
@@ -63,6 +158,33 @@ export default function Home() {
           </p>
         </div>
       </footer>
-    </main>
+
+      <footer className="md:hidden">
+        <div className="">
+          <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
+            Made with ❤️ by{" "}
+            <a
+              className="hover:text blue-900 font-extrabold text-blue-700"
+              href="https://twitter.com/miguelgargallo"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @miguelgargallo
+            </a>
+          </p>
+
+          <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
+            <a
+              className="hover:text blue-900 font-extrabold text-blue-700"
+              href="https://twitter.com/pencildomains"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @pencildomains
+            </a>
+          </p>
+        </div>
+      </footer>
+    </div>
   );
 }
