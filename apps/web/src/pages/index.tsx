5494a75d517a8b030ed014a1cf6a669242d9fd76
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-11-15 19:11:17 +0100

    A 1.0.7 v2 version on doc

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home() {
                     Pencil Explorer
                   </a>{" "}
                   <a className="badge m-2 bg-green-200 py-0 px-2 text-black">
-                    Alpha v1.0.6
+                    Alpha v1.0.7
                   </a>
                   <p className="py-4 text-sm text-gray-500">
                     Block Explorer and Naming Tool for Handshake, the
