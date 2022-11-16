f177a9b2c2756251a1998ce0aa2a22fdc21b9fe9
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-11-16 20:33:16 +0100

    A 1.0.15 the beggining

---
+++ packages/ui/src/Title.tsx
@@ -0,0 +1,29 @@
+export const Title = () => {
+  <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div className="py-8">
+      <div className="mx-auto max-w-7xl">
+        <div className="overflow-hidden rounded-lg bg-white">
+          <div className="px-4 py-5 text-center sm:p-6">
+            <h1 className="text-3xl font-bold leading-tight text-gray-900">
+              <a
+                href="https://twitter.com/pencildomains"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-blue text-indigo-600"
+              >
+                Pencil Explorer
+              </a>{" "}
+              <a className="badge m-2 bg-green-200 py-0 px-2 text-black">
+                Alpha v1.0.8
+              </a>
+              <p className="py-4 text-sm text-gray-500">
+                Block Explorer and Naming Tool for Handshake, the decentralized,
+                open, and extensible naming system for the Internet.
+              </p>
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>;
+};
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -2,3 +2,5 @@ import * as React from "react";
 export * from "./Button";
 export * from "./Head";
 export * from "./Search";
+export * from "./Title";
+export * from "./Title";
