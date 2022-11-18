a0b1a42306374eaac5d8cb283c31314d651ec3d6
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-11-18 19:04:33 +0100

    1.0.14 A beautiful logo

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -51,67 +51,59 @@ export default function Home() {
                   </p>
                 </h1>
                 <div className="bg-white py-8 text-center">
-                <a
-            href="https://handshake.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="p-4"
-          >
-            <picture>
-              <img
-                src="https://handshake.org/images/landing/logo-dark.svg"
-                alt="Landscape picture"
-                width={75}
-                height={20}
-              />
-            </picture>
-          </a>
-                <div className="mt-2 max-w-xl text-sm text-gray-500"></div>
-                <div className="mt-5">
-                  <div className="mt-1">
-                    <div className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-center text-base font-bold text-blue-500 shadow-xl hover:bg-white hover:text-blue-500 hover:shadow-md md:py-4 md:px-10 md:text-lg">
-                      <svg className="w-6 h-6 mr-2" viewBox="0 0 24 24">
-                        <path
-                          fill="currentColor"
-                          d="M12,2C6.48,2 2,6.48 2,12C2,17.52 6.48,22 12,22C17.52,22 22,17.52 22,12C22,6.48 17.52,2 12,2M12,20C7.58,20 4,16.42 4,12C4,7.58 7.58,4 12,4C16.42,4 20,7.58 20,12C20,16.42 16.42,20 12,20M11,7H13V13H11V7M11,15H13V17H11V15Z"
-                        />
-                      </svg>
-                      
+                  <a
+                    href="https://handshake.org/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="p-4"
+                  >
+                    <picture>
+                      <img
+                        src="https://handshake.org/images/landing/logo-dark.svg"
+                        alt="Landscape picture"
+                        width={150}
+                        height={20}
+                        className="justify-center mx-auto"
+                      />
+                    </picture>
+                  </a>
+                  <div className="mt-2 max-w-xl text-sm text-gray-500"></div>
+                  <div className="mt-5">
+                    <div className="mt-1">
+                     <input
+                        className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-center text-base font-bold text-blue-500 shadow-xl hover:bg-white hover:text-blue-500 hover:shadow-md md:py-4 md:px-10 md:text-lg"
+                        type="text"
+                        aria-label="Type & press enter ↵"
+                        placeholder="Type & press enter ↵"
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        onKeyPress={(e) => {
+                          if (e.key === "Enter") {
+                            console.log(getData());
+                          }
+                        }}
+                      />
                     </div>
-
-                    <input
-                      className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-center text-base font-bold text-blue-500 shadow-xl hover:bg-white hover:text-blue-500 hover:shadow-md md:py-4 md:px-10 md:text-lg"
-                      type="text"
-                      aria-label="Type & press enter ↵"
-                      placeholder="Type & press enter ↵"
-                      value={input}
-                      onChange={(e) => setInput(e.target.value)}
-                      onKeyPress={(e) => {
-                        if (e.key === "Enter") {
-                          console.log(getData());
-                        }
-                      }}
-                    />
+                    <div className="mx-auto mt-6 grid max-w-lg gap-5 md:hidden lg:max-w-xl lg:grid-cols-1">
+                      <button
+                        className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-base font-bold text-blue-500 shadow-md hover:bg-white hover:text-blue-500 hover:shadow-xl md:py-4 md:px-10 md:text-lg"
+                        onClick={() => console.log(getData())}
+                      >
+                        Explore Now, avoid caplocks
+                      </button>
+                    </div>
                   </div>
-                  <div className="mx-auto mt-6 grid max-w-lg gap-5 md:hidden lg:max-w-xl lg:grid-cols-1">
-                    <button
-                      className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-base font-bold text-blue-500 shadow-md hover:bg-white hover:text-blue-500 hover:shadow-xl md:py-4 md:px-10 md:text-lg"
-                      onClick={() => console.log(getData())}
-                    >
-                      Explore Now, avoid caplocks
-                    </button>
-                  </div>
-                </div>
-                <div className="mt-5">
-                  <div className="mt-1">
-                    <textarea
-                      id="about"
-                      name="about"
-                      rows={25}
-                      className="focus:border-blue-500-500 focus:ring-blue-500-500 mt-1 block w-full rounded-md border-gray-300 shadow-sm sm:text-sm"
-                      value={JSON.stringify(data, null, 2)}
-                      readOnly
-                    />
+                  <div className="mt-5">
+                    <div className="mt-1">
+                      <textarea
+                        id="about"
+                        name="about"
+                        rows={25}
+                        className="focus:border-blue-500-500 focus:ring-blue-500-500 mt-1 block w-full rounded-md border-gray-300 shadow-sm sm:text-sm"
+                        value={JSON.stringify(data, null, 2)}
+                        readOnly
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
