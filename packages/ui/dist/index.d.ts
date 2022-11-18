c05a37c28e44896ab978ad7c7b2dc90dad123f63
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-11-18 17:23:41 +0100

    1.0.13 corrected color

--- packages/ui/dist/index.d.ts
+++ packages/ui/dist/index.d.ts
@@ -2,6 +2,4 @@ declare const Button: () => JSX.Element;
 
 declare const Footer: () => JSX.Element;
 
-declare const hnsLogo: () => JSX.Element;
-
-export { Button, Footer, hnsLogo };
+export { Button, Footer };
