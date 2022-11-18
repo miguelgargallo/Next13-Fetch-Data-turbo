720e87b5fb18c9cb8efd5ea74cf0876f1530e722
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-11-18 17:07:04 +0100

    1.0.13 Added Handshake Logo

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import { useState } from "react";
 import { Footer } from "ui";
-import { Svg } from "ui";
 
 export default function Home() {
   const [input, setInput] = useState("");
@@ -55,8 +54,15 @@ export default function Home() {
                 <div className="mt-5">
                   <div className="mt-1">
                     <div className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-center text-base font-bold text-blue-500 shadow-xl hover:bg-white hover:text-blue-500 hover:shadow-md md:py-4 md:px-10 md:text-lg">
-                     <Svg />
+                      <svg className="w-6 h-6 mr-2" viewBox="0 0 24 24">
+                        <path
+                          fill="currentColor"
+                          d="M12,2C6.48,2 2,6.48 2,12C2,17.52 6.48,22 12,22C17.52,22 22,17.52 22,12C22,6.48 17.52,2 12,2M12,20C7.58,20 4,16.42 4,12C4,7.58 7.58,4 12,4C16.42,4 20,7.58 20,12C20,16.42 16.42,20 12,20M11,7H13V13H11V7M11,15H13V17H11V15Z"
+                        />
+                      </svg>
+                      
                     </div>
+
                     <input
                       className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-center text-base font-bold text-blue-500 shadow-xl hover:bg-white hover:text-blue-500 hover:shadow-md md:py-4 md:px-10 md:text-lg"
                       type="text"
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,4 +1,3 @@
 import * as React from "react";
 export * from "./Button";
 export * from "./Footer";
-export * from "./Svg";
