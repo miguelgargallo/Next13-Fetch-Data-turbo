b3bff6ba7190e718d460f6074a78d12a8473b9e2
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-11-19 17:50:01 +0100

    1.1.0 rev9 Exporting outside resources

--- packages/ui/dist/index.d.ts
+++ packages/ui/dist/index.d.ts
@@ -2,6 +2,4 @@ declare const Button: () => JSX.Element;
 
 declare const Footer: () => JSX.Element;
 
-declare const Darkfooter: () => JSX.Element;
-
-export { Button, Darkfooter, Footer };
+export { Button, Footer };
--- packages/ui/dist/index.js
+++ packages/ui/dist/index.js
@@ -21,7 +21,6 @@ var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: tru
 var src_exports = {};
 __export(src_exports, {
   Button: () => Button,
-  Darkfooter: () => Darkfooter,
   Footer: () => Footer
 });
 module.exports = __toCommonJS(src_exports);
@@ -142,107 +141,8 @@ var Footer = () => {
     ]
   });
 };
-
-// src/Darkfooter.tsx
-var import_jsx_runtime3 = require("react/jsx-runtime");
-var Darkfooter = () => {
-  return /* @__PURE__ */ (0, import_jsx_runtime3.jsxs)("footer", {
-    className: "bg-black py-8 text-center",
-    children: [
-      /* @__PURE__ */ (0, import_jsx_runtime3.jsx)("div", {
-        children: /* @__PURE__ */ (0, import_jsx_runtime3.jsxs)("a", {
-          className: "text-white",
-          children: [
-            "Made with \u2764\uFE0F by",
-            " ",
-            /* @__PURE__ */ (0, import_jsx_runtime3.jsx)("a", {
-              href: "https://twitter.com/miguelgargallo",
-              target: "_blank",
-              rel: "noopener noreferrer",
-              className: "hover:text-blue text-blue-600",
-              children: "Miguel Gargallo"
-            }),
-            "."
-          ]
-        })
-      }),
-      /* @__PURE__ */ (0, import_jsx_runtime3.jsxs)("div", {
-        children: [
-          /* @__PURE__ */ (0, import_jsx_runtime3.jsxs)("a", {
-            className: "text-white",
-            children: [
-              "2022 \xA9",
-              " ",
-              /* @__PURE__ */ (0, import_jsx_runtime3.jsx)("a", {
-                href: "https://huggingface.co/spaces/superdatas/LICENSE",
-                target: "_blank",
-                rel: "noopener noreferrer",
-                className: "hover:text-blue text-blue-600",
-                children: "Pylar AI creative ML license"
-              }),
-              "."
-            ]
-          }),
-          /* @__PURE__ */ (0, import_jsx_runtime3.jsxs)("div", {
-            className: "mt-2",
-            style: {
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center"
-            },
-            children: [
-              /* @__PURE__ */ (0, import_jsx_runtime3.jsx)("a", {
-                href: "https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fmiguelgargallo%2FNext13-Fetch-Data-turbo&project-name=miguelgargallo-next13-fetch-data-turbo&repo-name=miguelgargallo-next13-fetch-data-turbo",
-                target: "_blank",
-                rel: "noopener noreferrer",
-                className: "p-4",
-                children: /* @__PURE__ */ (0, import_jsx_runtime3.jsx)("picture", {
-                  children: /* @__PURE__ */ (0, import_jsx_runtime3.jsx)("img", {
-                    src: "https://vercel.com/button",
-                    alt: "Landscape picture",
-                    width: 75,
-                    height: 20
-                  })
-                })
-              }),
-              /* @__PURE__ */ (0, import_jsx_runtime3.jsx)("a", {
-                href: "https://huggingface.co/spaces/superdatas/LICENSE",
-                target: "_blank",
-                rel: "noopener noreferrer",
-                className: "p-4",
-                children: /* @__PURE__ */ (0, import_jsx_runtime3.jsx)("picture", {
-                  children: /* @__PURE__ */ (0, import_jsx_runtime3.jsx)("img", {
-                    src: "https://huggingface.co/front/assets/huggingface_logo.svg",
-                    alt: "Landscape picture",
-                    width: 40,
-                    height: 20
-                  })
-                })
-              }),
-              /* @__PURE__ */ (0, import_jsx_runtime3.jsx)("a", {
-                href: "https://github.com/miguelgargallo/Next13-Fetch-Data-turbo",
-                target: "_blank",
-                rel: "noopener noreferrer",
-                className: "p-4",
-                children: /* @__PURE__ */ (0, import_jsx_runtime3.jsx)("picture", {
-                  children: /* @__PURE__ */ (0, import_jsx_runtime3.jsx)("img", {
-                    src: "https://img.shields.io/github/stars/miguelgargallo/Next13-Fetch-Data-turbo?style=social",
-                    alt: "Landscape picture",
-                    width: 100,
-                    height: 20
-                  })
-                })
-              })
-            ]
-          })
-        ]
-      })
-    ]
-  });
-};
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
   Button,
-  Darkfooter,
   Footer
 });
