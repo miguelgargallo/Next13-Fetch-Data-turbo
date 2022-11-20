daf6f370a095366216dad217eb7908526e89f246
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-11-20 14:18:54 +0100

    1.2.3 Styles on Grading Systems

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import { useState } from "react";
+import { Gradient } from "ui";
+import { motion } from "framer-motion";
 import { Footer } from "ui";
 
 export default function Home() {
@@ -11,6 +13,13 @@ export default function Home() {
     const json = await res.json();
     setData(json);
     console.log(json);
+  };
+
+  const getPylar = async () => {
+    const ans = await fetch(`https://e.hnsfans.com/api/names/${input}`);
+    const data = await res.json();
+    setPylar(json);
+    console.log(data);
   };
 
   if (list.includes(input)) {
@@ -81,7 +90,7 @@ export default function Home() {
                         rel="noopener noreferrer"
                         className="text-white hover:text-gray-200"
                       >
-                        <a>v 1.2.2 Styles</a>
+                        <a>v 1.2.3 Styles</a>
                       </a>
                     </a>
                     <a>Introducing a new Grading System for Handshake TLDs.</a>
@@ -104,79 +113,90 @@ export default function Home() {
                           }
                         }}
                       />
-                    </div>
-                    <div className="mt-1 py-2">
-                      {list.map((item) => {
-                        if (item === input) {
-                          return (
-                            <div className="mt-1 py-2">
-                              <p className="text-black">
-                                .{input} is a valid TLD, on the list is the
-                                number {list.indexOf(input)}
-                                <p>
-                                  <a
-                                    href="https://raw.githubusercontent.com/miguelgargallo/Next13-Fetch-Data-turbo/main/apps/web/src/pages/api/list.pylar"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    className="text-blue-500 hover:text-yellow-500"
-                                  >
-                                    {" "}
-                                    <a>Pylar AI list: source list.pylar</a>
-                                  </a>
-                                </p>
-                              </p>
-                            </div>
-                          );
-                        }
-                      })}
-                    </div>
-                    <div className="mt-1 py-2">
-                      {input.length > 0 && input.length < 8 && (
-                        <div>
-                          .{input} has {input.length} characters, this is
-                          <a>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" stroke="#0fa9eb" stroke-width="0" viewBox="0 0 38.012 39.702" fill="linearGradient" color="#0fa9eb">
-    <path d="M32.816,12.95,30.349,8.568h4.78a.439.439,0,0,1,.346.2l.931,1.534c.558.92,1.225,2.02,1.607,2.648ZM23.975,39.495a.339.339,0,0,1-.315.207H18.772l7.595-13.635a.731.731,0,0,0-.636-1.087h0L12.787,25l-2.574-4.456H29.076c.011,0,.02,0,.031,0a.677.677,0,0,0,.217-.045l.047-.02a.722.722,0,0,0,.346-.313l3.108-5.745h5.13Zm-6.468-.518-.613-1.009c-.785-1.3-1.791-2.955-1.9-3.137a.385.385,0,0,1,.024-.355l4.457-8.027,5.019-.006ZM8.948,30.444,6.453,26.011,8.968,21.3l2.522,4.367C10.706,27.14,9.483,29.442,8.948,30.444Zm-3.348.69H2.881a.443.443,0,0,1-.343-.2L1.726,29.6C1.147,28.641.409,27.425,0,26.752H5.2l2.466,4.383ZM14.037.208A.338.338,0,0,1,14.351,0h4.91L11.646,13.636c-.006.011-.01.023-.016.035s-.021.046-.03.07-.015.046-.021.069-.011.044-.014.066a.648.648,0,0,0-.008.081c0,.012,0,.023,0,.036s0,.018,0,.027a.591.591,0,0,0,.008.079.538.538,0,0,0,.012.066c.006.023.013.045.021.068s.016.045.025.066.021.041.033.062.023.04.037.059a.644.644,0,0,0,.045.055c.016.017.031.035.048.051s.035.029.053.043a.634.634,0,0,0,.063.045l.023.016c.008,0,.018.006.027.011a.729.729,0,0,0,.119.048c.015,0,.029.01.044.013a.7.7,0,0,0,.164.021h.33l12.619-.017c.334.58.957,1.673,1.489,2.6q.612,1.072,1.01,1.768H8.949c-.008,0-.016,0-.024,0a.725.725,0,0,0-.631.382L5.185,25.29H.059Zm6.479.534.456.75c.8,1.321,1.932,3.185,2.052,3.379A.383.383,0,0,1,23,5.227l-4.457,8.027-5.017.006Zm8.546,8.511,2.5,4.435-2.545,4.7c-.3-.529-.68-1.189-1.034-1.808-.7-1.224-1.208-2.115-1.495-2.613C27.028,12.977,28.462,10.349,29.062,9.253Z" fill="#000" fill-rule="evenodd" id="Fill-1"></path>
-</svg>
-                            {input.length > 0 && input.length < 4 && (
-                              <a className="tld_diamond"> a 💎 Diamond TLD.</a>
-                            )}
-                          </a>
-                          <a>
-                            {input.length > 3 && input.length < 6 && (
-                              <a className="tld_gold"> a 🥇 Gold TLD.</a>
-                            )}
-                          </a>
-                          <a>
-                            {input.length > 5 && input.length < 7 && (
-                              <a className="tld_silver"> a 🥈 Silver TLD.</a>
-                            )}
-                          </a>
-                          <a>
-                            {input.length > 6 && input.length < 8 && (
-                              <a className="tld_bronze"> a 🥉 Bronze TLD.</a>
-                            )}
-                          </a>
+                      <div className="flex w-full items-center justify-center rounded-md px-8 py-2 text-center text-base font-bold text-blue-500 shadow-xl hover:text-blue-500 hover:shadow-md md:py-2 md:px-10 md:text-lg">
+                        <div className="mt-1 py-2">
+                          <Gradient />
                         </div>
-                      )}
-                      <p className="text-sm text-gray-500">
-                        .
-                        {input.length > 7 && (
-                          <a>
-                            This is longer than 7 characters, if you strongly
-                            believe in this name, please consider{" "}
-                            <a
-                              href="https://github.com/miguelgargallo/Next13-Fetch-Data-turbo/issues"
-                              target="_blank"
-                              rel="noopener noreferrer"
-                              className="hyperlink_issue"
-                            >
-                              opening an issue
-                            </a>{" "}
-                            to discuss it.
-                          </a>
-                        )}
-                      </p>
+                        <div className="mt-1 py-2">
+                          {list.map((item) => {
+                            if (item === input) {
+                              return (
+                                <div className="mt-1 py-2">
+                                  <p className="text-black">
+                                    .{input} is a valid TLD, on the list is the
+                                    number {list.indexOf(input)}
+                                    <p>
+                                      <a
+                                        href="https://raw.githubusercontent.com/miguelgargallo/Next13-Fetch-Data-turbo/main/apps/web/src/pages/api/list.pylar"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="text-blue-500 hover:text-yellow-500"
+                                      >
+                                        {" "}
+                                        <a>Pylar AI list: source list.pylar</a>
+                                      </a>
+                                    </p>
+                                  </p>
+                                </div>
+                              );
+                            }
+                          })}
+                        </div>
+                        <div className="mt-1 py-2">
+                          {input.length > 0 && input.length < 8 && (
+                            <div>
+                              .{input} has {input.length} characters, this is
+                              <a>
+                                {input.length > 0 && input.length < 4 && (
+                                  <a className="tld_diamond">
+                                    {" "}
+                                    a 💎 Diamond TLD.
+                                  </a>
+                                )}
+                              </a>
+                              <a>
+                                {input.length > 3 && input.length < 6 && (
+                                  <a className="tld_gold"> a 🥇 Gold TLD.</a>
+                                )}
+                              </a>
+                              <a>
+                                {input.length > 5 && input.length < 7 && (
+                                  <a className="tld_silver">
+                                    {" "}
+                                    a 🥈 Silver TLD.
+                                  </a>
+                                )}
+                              </a>
+                              <a>
+                                {input.length > 6 && input.length < 8 && (
+                                  <a className="tld_bronze">
+                                    {" "}
+                                    a 🥉 Bronze TLD.
+                                  </a>
+                                )}
+                              </a>
+                            </div>
+                          )}
+                          <p className="text-sm text-gray-500">
+                            .
+                            {input.length > 7 && (
+                              <a>
+                                This is longer than 7 characters, if you
+                                strongly believe in this name, please consider{" "}
+                                <a
+                                  href="https://github.com/miguelgargallo/Next13-Fetch-Data-turbo/issues"
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                  className="hyperlink_issue"
+                                >
+                                  opening an issue
+                                </a>{" "}
+                                to discuss it.
+                              </a>
+                            )}
+                          </p>
+                        </div>
+                      </div>
 
                       <div className="button">
                         <button
