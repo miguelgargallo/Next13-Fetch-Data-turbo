2d5ffdfdab180c1430c58f70a830541c903be1b4
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-11-21 00:01:39 +0100

    v 1.4.3 Developers

--- apps/web/src/pages/developer.tsx
+++ apps/web/src/pages/developer.tsx
@@ -13,10 +13,22 @@ export default function Developer() {
       <Menu />
       <MenuDevs />
       <a className="m-4 p-4 text-center text-xl font-bold text-black">
-      Every reason to turn your TLD into a Business.™
+        Every reason to turn your TLD into a Business.™
       </a>
       <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-
+        <div className="grid grid-cols-3 gap-4">
+          <div className="rounded-lg bg-white p-4 shadow-md">
+            <h3 className="text-2xl font-bold text-gray-800">
+              Developers cut 75% Pencil 25% Every SLDs sell.
+            </h3>
+            <h3 className="text-2xl font-bold text-gray-800">
+              Revenue every 80$ or in $HNS on Tuesdays.
+            </h3>
+            <h3 className="text-2xl font-bold text-gray-800">
+              Send the TLD & Contact Twitter now.
+            </h3>
+          </div>
+        </div>
       </main>
       <Footer />
     </div>
