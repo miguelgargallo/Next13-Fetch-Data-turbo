511f2035b537b2cd5b3f2503b3f18cf8a21d6c36
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-11-23 12:46:03 +0100

    1.7.3

--- packages/ui/src/Analytics.tsx
+++ packages/ui/src/Analytics.tsx
@@ -1,21 +1,16 @@
-import Script from 'next/script'
-
 function Analytics() {
+
   return (
     <div className="container">
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-BKMWFDV3F4"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
 
-          gtag('config', 'G-BKMWFDV3F4');
-        `}
-      </Script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BKMWFDV3F4"> </script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){window.dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-BKMWFDV3F4');
+</script>
+
     </div>
   )
 }
