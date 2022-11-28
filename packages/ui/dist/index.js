02c0f70e55f4dc72ca6ec85043abd6e137fdac17
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-11-28 10:57:59 +0100

    1.8.2 Adding staff

--- packages/ui/dist/index.js
+++ packages/ui/dist/index.js
@@ -1034,7 +1034,7 @@ var Version = () => {
       href: "http://github.com/miguelgargallo/next13-Fetch-Data-turbo",
       target: "_blank",
       rel: "noopener noreferrer",
-      children: "v1.8.1"
+      children: "v1.8.2"
     })
   });
 };
