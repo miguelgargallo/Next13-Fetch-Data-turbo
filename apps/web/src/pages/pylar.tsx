304b5c7cc8d72165813d5bcf34285123fe876c21
Miguel Gargallo <mgargallo16@gmail.com>
2022-12-04 16:45:05 +0100

    1.0.1

--- apps/web/src/pages/pylar.tsx
+++ apps/web/src/pages/pylar.tsx
@@ -4,6 +4,7 @@ import { Menu } from "ui";
 import { TitlePylar } from "ui";
 import { Footer } from "ui";
 import { MenuPylar } from "ui";
+
 export default function Home() {
   console.log("✅ Pylar AI");
   return (
@@ -14,18 +15,17 @@ export default function Home() {
       </Head>
       <Menu />
       <MenuPylar />
-
       <main>
       <TitlePylar />
         <div className="pylarDiv">
         <img
             className="pylarDimension"
-            src="https://raw.githubusercontent.com/miguelgargallo/Next13-Gallery-Image-Turbo/main/Data.pylar/Images/Pylar-v1.1/A_photography_of_the_earth__from_the_moon_on_a_fis_AAAGOLjw_RealESRGAN_x4plus.jpeg"
+            src="https://raw.githubusercontent.com/miguelgargallo/Next13-Fetch-Data-turbo/main/Data.pylar/Images/Pylar-v1.0/A_photography_of_the_earth__from_the_moon_on_a_fis_AAAGOLpA_RealESRGAN_x4plus.jpeg"
             alt="Pylar AI"
           />
           <img
             className="pylarDimension"
-            src="https://raw.githubusercontent.com/miguelgargallo/Next13-Gallery-Image-Turbo/main/Data.pylar/Images/Pylar-v1.1/A_photography_of_the_earth__from_the_moon_on_a_fis_AAAGOLjw_RealESRGAN_x4plus.jpeg"
+            src="https://raw.githubusercontent.com/miguelgargallo/Next13-Fetch-Data-turbo/main/Data.pylar/Images/Pylar-v1.1/A_photography_of_the_earth__from_the_moon_on_a_fis_AAAGOLjw_RealESRGAN_x4plus.jpeg"
             alt="Pylar AI"
           />
         </div>
