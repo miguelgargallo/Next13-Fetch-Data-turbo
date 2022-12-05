33f69799a1fabfde4787c028e58343cdc0002333
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-12-05 20:22:27 +0100

    1.9.8 Addons

--- packages/ui/src/Menu.tsx
+++ packages/ui/src/Menu.tsx
@@ -1,8 +1,8 @@
 import { Version } from "./Version";
 import { Supermenu } from "./Supermenu";
-import { MenuLogo } from "./MenuLogo";
 import { motion, useScroll } from "framer-motion";
 import "./styles.css";
+import { PencilLogo } from "./PencilLogo";
 
 {
   // show hour and minute locale
@@ -23,7 +23,7 @@ export const Menu = () => {
       <Supermenu />
 
       <div className="flex flex-row items-center justify-between bg-gradient-to-r from-blue-400 to-blue-500">
-        <MenuLogo />{" "}
+        <PencilLogo />{" "}
         <button className="rounded-xl py-2 px-4 font-bold text-white">
           <a href="/explorer">Explorer</a>
         </button>
