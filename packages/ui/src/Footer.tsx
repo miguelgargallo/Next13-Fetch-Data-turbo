4b776b8a1f099273ed06309d0ecfcfe2ab34a208
miguelgargallo <mgargallo@gmail.com>
2022-12-12 15:08:33 +0100

    1.0.0

--- packages/ui/src/Footer.tsx
+++ packages/ui/src/Footer.tsx
@@ -1,6 +1,8 @@
+import { Versions } from "./Versions";
+
 export const Footer = () => {
   return (
-    <footer className="bg-white py-8 text-center">
+    <footer className="bg-white py-8 text-center text-black">
       <div>
         <a>
           Made with ❤️ by{" "}
@@ -8,7 +10,7 @@ export const Footer = () => {
             href="https://twitter.com/miguelgargallo"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-blue text-indigo-600"
+            className="hover:text-blue text-black"
           >
             Miguel Gargallo
           </a>
@@ -22,7 +24,7 @@ export const Footer = () => {
             href="https://huggingface.co/spaces/superdatas/LICENSE"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-blue text-indigo-600"
+            className="hover:text-blue text-black"
           >
             Pylar&trade; AI creative ML license
           </a>
@@ -46,11 +48,36 @@ export const Footer = () => {
               />
             </picture>
           </a>
+          <Versions />
+          <div
+            className="hidden py-2 px-4 font-bold text-white md:block"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {" "}
+            <button className="hidden py-2 px-4 font-bold text-black md:block">
+              <a
+                href="http://twitter.com/miguelgargallo"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Twitter
+              </a>
+            </button>
+          </div>
         </div>
+
+        <p
+          className="text-black"
+        >Pencil Domains, The Fastest Domains.™</p>
       </div>
       <p>Please do not consider the score as investment advice.</p>
     </footer>
   );
+<<<<<<< HEAD
 };
 
 
@@ -71,3 +98,6 @@ export const Footer = () => {
             </picture>
           </a>
 */
+=======
+};
+>>>>>>> 00d4fb9 (1.9.99 Standard Footer with sentence)
