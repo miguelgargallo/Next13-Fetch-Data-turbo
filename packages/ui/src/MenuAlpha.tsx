7cbe35ce9808eb0c1822b1da930196d6a42c687e
Miguel Gargallo <mgargallo16@gmail.com>
2022-12-18 14:51:26 +0100

    2.0.1 Menu

--- packages/ui/src/MenuAlpha.tsx
+++ packages/ui/src/MenuAlpha.tsx
@@ -2,9 +2,9 @@ export const MenuAlpha = () => {
 
   return (
     <div className="flex flex-row items-center justify-between bg-gradient-to-r from-blue-400 to-blue-500">
-      <button className="px-8 py-2 font-bold text-white">
-        <a href="https://superdatas.org"
-        >SuperData</a>
+      <button className="px-4 py-2 underline font-bold text-white">
+        <a href="https://pencil.li"
+        >Pencil</a>
       </button>
       <button className="py-2 font-bold text-white">
         <a href="https://pylar.org"
@@ -14,9 +14,9 @@ export const MenuAlpha = () => {
         <a href="https://itamaesan.org"
         >ITAMAESAN</a>
       </button>
-      <button className="px-8 py-2 underline font-bold text-white">
-        <a href="https://pencil.li"
-        >Pencil</a>
+      <button className="px-4 py-2 font-bold text-white">
+        <a href="https://superdatas.org"
+        >SuperData</a>
       </button>
     </div>
   );
